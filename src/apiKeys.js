56063aaea25b4ebda187b10318e0264b42b2aec4
Sydney Noh <43580474+simplesnoh@users.noreply.github.com>
2022-11-13 10:14:41 -0600

    Delete apiKeys.js

--- src/apiKeys.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDPKjVCbEfepwQkQDPQHwiOuQYNYlD-FPU",
-  authDomain: "fir-demo-4d8dd.firebaseapp.com",
-  projectId: "fir-demo-4d8dd",
-  storageBucket: "fir-demo-4d8dd.appspot.com",
-  messagingSenderId: "527082919196",
-  appId: "1:527082919196:web:bf3453c8fb9e0796e7d6d9",
-};
